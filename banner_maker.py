9faa3a1d21c9be06361004b71c20381ad89943a3
Sardin <behdin.adrianos@gmail.com>
2019-12-03 11:26:30 +0330

    banner_maker by Alireza-Mani

---
+++ banner_maker.py
@@ -0,0 +1,22 @@
+from pyfiglet import Figlet
+import os , colorama
+
+
+def bannermaker():
+    os.system("cls")
+    colorama.init()
+    print(f"{color}{part1}{reset}")
+
+#font = Figlet(font="graffiti")
+#part1 = font.renderText(str(input("[*]Enter Your Charachters > ")))
+#color = colorama.Fore.LIGHTMAGENTA_EX
+#reset = colorama.Fore.RESET
+
+while(True):
+    os.system("cls")
+    font = Figlet(font="graffiti")
+    part1 = font.renderText(str(input("[*]Enter Your Charachters > ")))
+    color = colorama.Fore.LIGHTMAGENTA_EX
+    reset = colorama.Fore.RESET
+    bannermaker()
+    input(" \"this made by Alireza-Mani!! ;)\" press 'Enter' to make another!")
